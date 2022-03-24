fe43b52778f1a2ae67126f303fdc809b5ebeb570
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-03-24 21:24:46 +0900

    Create api_response class

---
+++ lib/model/api_response.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'api_response.freezed.dart';
+part 'api_response.g.dart';
+
+@freezed
+class ApiResponse with _$ApiResponse {
+  const factory ApiResponse({
+    required String code,
+  }) = _ApiResponse;
+
+  factory ApiResponse.fromJson(Map<String, dynamic> json) =>
+      _$ApiResponseFromJson(json);
+}
