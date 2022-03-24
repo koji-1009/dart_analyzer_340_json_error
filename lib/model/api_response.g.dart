6cc77cc00f51fde8a0cc0dfa5cd0c1b5ed6dc851
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-03-24 21:30:13 +0900

    Analyzer 3.3.1

---
+++ lib/model/api_response.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ApiResponse _$$_ApiResponseFromJson(Map<String, dynamic> json) =>
+    _$_ApiResponse(
+      code: json['code'] as String,
+    );
+
+Map<String, dynamic> _$$_ApiResponseToJson(_$_ApiResponse instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+    };
